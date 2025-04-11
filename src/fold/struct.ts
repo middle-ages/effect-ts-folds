10a18f430142bfb3c05bb16359407161506787fc
middle-ages <ran@haag59.com>
2025-04-10 23:10:30 -0700

    Improved struct fusion types

--- src/fold/struct.ts
+++ src/fold/struct.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {Covariant as CO} from '@effect/typeclass'
-import {pipe} from 'effect'
+import {pipe, Types} from 'effect'
 import {Kind, TypeLambda} from 'effect/HKT'
 import {Algebra} from './folds.js'
 
-export type ReturnTypes<
+export type StructReturns<
   F extends TypeLambda,
   S extends Record<string, Algebra<F, any>>,
   E = unknown,
@@ -20,23 +20,22 @@ export type ReturnTypes<
  */
 export const struct =
   <F extends TypeLambda>(F: CO.Covariant<F>) =>
-  <S extends Record<string, Algebra<F, any>>>(struct: S) => {
+  <const S extends Record<string, Algebra<F, any>>>(struct: S) => {
     type Key = keyof S
-    type Returns<E, R, I> = ReturnTypes<F, S, E, R, I>
 
     return <E = unknown, R = unknown, I = never>(
-      fas: Kind<F, I, R, E, Returns<E, R, I>>,
-    ): Returns<E, R, I> => {
-      const result = {} as Returns<E, R, I>
+      fas: Kind<F, I, R, E, StructReturns<F, S, E, R, I>>,
+    ): Types.Simplify<StructReturns<F, S, E, R, I>> => {
+      const result = {} as StructReturns<F, S, E, R, I>
 
       for (const key of Object.keys(struct) as Key[]) {
         result[key] = pipe(
           fas,
           F.map(xs => xs[key]),
           struct[key] as Algebra<F, any>,
-        ) as Returns<E, R, I>[typeof key]
+        ) as StructReturns<F, S, E, R, I>[typeof key]
       }
 
-      return result
+      return result as Types.Simplify<StructReturns<F, S, E, R, I>>
     }
   }
